d67daa20207412846998d74154bf559e86cd394f
Gover Braam <groverbraam@gmail.com>
2022-01-31 14:46:19 -0500

    almost done at like 75% right now

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
 // console.log("corgi carousel connected to app.js!");
-// $(() => {
-// });
-$( "#dayNight-outline" ).on("click", function() {
-            if( $("body").hasClass("dark-mode")) {
-                $("body").removeClass("dark-mode");
-                $("#dayNight-outline").removeAttr("name");
-                $("#dayNight-outline").attr("name", "moon-outline");
-            } else {
-                $("body").addClass("dark-mode");
-                $("#dayNight-outline").removeAttr("name");
-                $("#dayNight-outline").attr("name", "sunny-outline");
-            }
+$(() => {
+  $( "#dayNight-outline" ).on("click", () => {
+              if( $("body").hasClass("dark-mode")) {
+                  $("body").removeClass("dark-mode");
+                  $("#dayNight-outline").removeAttr("name");
+                  $("#dayNight-outline").attr("name", "moon-outline");
+              } else {
+                  $("body").addClass("dark-mode");
+                  $("#dayNight-outline").removeAttr("name");
+                  $("#dayNight-outline").attr("name", "sunny-outline");
+              }
+  });
+
+  currentImgIndex = 0;
+  numOfImages = $('.carousel-images').children().length - 1
+  $('.next').on('click', () => {
+      $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+      if(currentImgIndex < numOfImages) {
+        currentImgIndex++
+      } else {
+        currentImgIndex = 0
+      }
+      $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+      })
+
+      $('.previous').on('click', () => {
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'none')
+        if(currentImgIndex > 0) {
+          currentImgIndex--
+        } else {
+          currentImgIndex = numOfImages
+        }
+        $('.carousel-images').children().eq(currentImgIndex).css('display', 'block')
+  })
+
+  const $open = $('#submit')
+  const $alert = $('#alert')
+  const $close = $('#close')
+
+  const openModal = () => {
+    $alert.css('display', 'flex')
+    // $alert.css('display', 'flex')
+  }
+
+  const closeModal = () => {
+    $alert.css('display', 'none')
+  }
+
+  $open.on('click', openModal)
+  $close.on('click', closeModal)
+
+
 });
