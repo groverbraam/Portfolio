204ba28f5d8c111734955cb6cedaa7b5b0288297
Gover Braam <groverbraam@gmail.com>
2022-01-31 22:41:58 -0500

    final changes for now... README done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Portfolio
+
+I used HTML, CSS, Javascript, and jQuery for my portfolio.
+I started with the HTML for each page then CSS, and ended with Javascript/jQuery.
+
+Link: https://xenodochial-swanson-1aa82c.netlify.app/
+
+I had trouble getting the day/night mode preference to stay when you switch pages.
+I also need to fix the message when you submit contact information, and make sure the text clears from the boxes.
+I need to add projects once I am proud of some that I have made and figure out how to get the text to change with the carousel.
+Also need to fix the spacing on my resume.
+Matt told me that after completing Unit 2 I will learn how to store the information
--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // console.log("corgi carousel connected to app.js!");
 $(() => {
   $( "#dayNight-outline" ).on("click", () => {
-              if( $("body").hasClass("dark-mode")) {
-                  $("body").removeClass("dark-mode");
-                  $("#dayNight-outline").removeAttr("name");
-                  $("#dayNight-outline").attr("name", "moon-outline");
-              } else {
-                  $("body").addClass("dark-mode");
-                  $("#dayNight-outline").removeAttr("name");
-                  $("#dayNight-outline").attr("name", "sunny-outline");
-              }
+      if( $("body").hasClass("dark-mode")) {
+        $("body").removeClass("dark-mode");
+        $("#dayNight-outline").removeAttr("name");
+        $("#dayNight-outline").attr("name", "moon-outline");
+      } else {
+          $("body").addClass("dark-mode");
+          $("#dayNight-outline").removeAttr("name");
+          $("#dayNight-outline").attr("name", "sunny-outline");
+        }
   });
 
   currentImgIndex = 0;
